50f87fcae43684cbe14c8cc37020d2f8b50836b4
Ahmet Alp Çınar <cinaralp32@gmail.com>
2024-08-30 14:21:33 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrialNT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/js/site.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.toLowerCase();
+    const navLinks = document.querySelectorAll('#mainNav .nav-link');
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute('href').toLowerCase();
+
+        if (currentPage === linkPage) {
+            link.parentElement.classList.add('active');
+        }
+        else {
+            link.parentElement.classList.remove('active');
+        }
+    });
+});
